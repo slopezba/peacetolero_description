7222d4ee7aa805767ff304ffbc90723c26c47346
slopezb <barajas@uji.es>
2024-12-16 17:42:44 +0100

    peacetolero description initial commit

---
+++ launch/description.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare 
+
+def generate_launch_description():
+    xacro_file = PathJoinSubstitution([
+        FindPackageShare("peacetolero_description"),
+        "urdf",
+        "urdf.xacro"
+    ])
+
+    return LaunchDescription([
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            parameters=[{
+                "robot_description": Command(["xacro ", xacro_file])
+            }]
+        )
+    ])
